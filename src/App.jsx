19fc41a4f924146a4c3249c3c4ecd98c7ddc6f98
Muhammad-Sajid-Rajput <muhammadsajidrajput20@gmail.com>
2025-12-30 01:35:23 +0500

    change simple pages to react pages with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
 import { HomePage } from './pages/HomePage.jsx'
+import { CheckoutPage } from './pages/checkout.jsx'
+import { OrdersPage } from './pages/orders.jsx'
+import { TrackingPage } from './pages/tracking.jsx'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
+    </Routes>
   )
 }
 
